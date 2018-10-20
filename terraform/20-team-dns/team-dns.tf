1b9429427b0acba8f268ee549f8a43faa94f4559
Marcel Boettcher <marcel.boettcher@otto.de>
2018-10-20 21:35:38 +0200

    Use newest NS records for team1

--- terraform/20-team-dns/team-dns.tf
+++ terraform/20-team-dns/team-dns.tf
@@ -2,11 +2,11 @@ resource "aws_route53_record" "team1" {
   zone_id = "${data.aws_route53_zone.root.id}"
   name = "team1.codelab.marcelboettcher.de"
   type = "NS"
-  ttl = 60
+  ttl = 60 // seconds
   records = [
-    "ns-179.awsdns-22.com.",
-    "ns-2001.awsdns-58.co.uk.",
-    "ns-1291.awsdns-33.org.",
-    "ns-576.awsdns-08.net."
+    "ns-1774.awsdns-29.co.uk.",
+    "ns-1206.awsdns-22.org.",
+    "ns-895.awsdns-47.net.",
+    "ns-228.awsdns-28.com."
   ]
 }
