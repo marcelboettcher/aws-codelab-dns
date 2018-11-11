1a34d76232c9f9bbbb6cee77388b7b67c693d9d1
Marcel Boettcher <marcel.boettcher@otto.de>
2018-11-11 22:16:36 +0100

    Set new ns records for team1

--- terraform/20-team-dns/team1.tf
+++ terraform/20-team-dns/team1.tf
@@ -4,9 +4,9 @@ resource "aws_route53_record" "team1" {
   type = "NS"
   ttl = 60 // seconds
   records = [
-    "ns-1940.awsdns-50.co.uk.",
-    "ns-1274.awsdns-31.org.",
-    "ns-894.awsdns-47.net.",
-    "ns-325.awsdns-40.com."
+    "ns-1609.awsdns-09.co.uk.",
+    "ns-1226.awsdns-25.org.",
+    "ns-937.awsdns-53.net.",
+    "ns-1.awsdns-00.com."
   ]
 }
